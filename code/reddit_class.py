3bb24803ddf93a76faf7f9a610a2b9f45e2f99a5
ketankokane94 <ketankokane94@gmail.com>
2019-04-06 16:38:06 -0400

    added code to get top 5 post of a given subreddit, sample json file

--- code/reddit_class.py
+++ code/reddit_class.py
@@ -1,7 +1,7 @@
 import praw
-reddit = praw.Reddit(client_id='YOUR_CLIENT_ID', client_secret="YOUR_SECRET",
-                     password='YOUR_PASSWORD', user_agent='USERNAME',
-                     username='USERNAME')
 
-for comment in reddit.subreddit('rit').stream.comments():
+reddit = praw.Reddit()
+
+for comment in reddit.subreddit('rit').hot(limit=5):
+
 	print(vars(comment))
