84cddf25b3bf460f1cf686fe6388538b2ad450d1
ketankokane94 <ketankokane94@gmail.com>
2019-04-29 15:26:42 -0400

    Update get_post_from_reddit.py

--- code/data_gethering/get_post_from_reddit.py
+++ code/data_gethering/get_post_from_reddit.py
@@ -5,20 +5,20 @@ data = []
 reddit = connect_to_reddit()
 i=0
 
-for submission in reddit.subreddit('documentaries').top(limit = 2000):
+for submission in reddit.subreddit('Frugal').top(limit = 2000):
     print(i)
     i += 1
     print(vars(submission))
     data.append({'seltext': submission.selftext, 'title': submission.title, 'subredditName':
-        'documentaries'})
+        'Frugal'})
 
-reddit = connect_to_reddit()
-
-for submission in reddit.subreddit('lifehacks').top(limit = 2000):
-    print(i)
-    i += 1
-    print(vars(submission))
-    data.append({'seltext': submission.selftext, 'title': submission.title, 'subredditName': 'lifehacks'})
+# reddit = connect_to_reddit()
+#
+# for submission in reddit.subreddit('lifehacks').top(limit = 2000):
+#     print(i)
+#     i += 1
+#     print(vars(submission))
+#     data.append({'seltext': submission.selftext, 'title': submission.title, 'subredditName': 'lifehacks'})
 
 
 with open('posts.json', 'w') as fp:
